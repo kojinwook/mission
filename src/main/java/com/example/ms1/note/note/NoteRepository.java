21e4a9b5480f14854b46299565dc3b856f560900
kojinwook <gojinug4@gmail.com>
2024-05-10 13:12:55 +0900

    msi1

--- src/main/java/com/example/ms1/note/note/NoteRepository.java
+++ src/main/java/com/example/ms1/note/note/NoteRepository.java
@@ -1,5 +1,6 @@
 package com.example.ms1.note.note;
 
+import com.example.ms1.note.note.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
